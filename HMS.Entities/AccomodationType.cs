40c12055ef4ef6586b483713ce5a0e83ad1134a3
anjua <anjua.github@gmail.com>
2020-02-14 10:25:17 +0700

    Add Basic Entities

---
+++ HMS.Entities/AccomodationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class AccomodationType
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+    }
+}
---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public int AccomodationID { get; set; }
+        public Accomodation Accomodation { get; set; }
+        public DateTime FromDate { get; set; }
+        public int Duration { get; set; }
+    }
+}
